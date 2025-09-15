4693b7e3b2750b4a98839535831ceac51bd93f9e
Vladimir Prusakov <dzhedaj@gmail.com>
2025-09-15 10:02:23 +0300

    example

--- examples/parser/nb_version_error.py
+++ examples/parser/nb_version_error.py
@@ -1,48 +1,93 @@
-"""Example NbValue raise NbVersionError."""
+"""Example NbValue raise NbVersionError, for migration from Netbox v3.5 to v4.3.
+"""
+from pprint import pprint
+
+import dictdiffer
+
 from nbforager import NbValue
-from nbforager.exceptions import NbVersionError
 
-# ipam/prefixes data for Netbox < v4.2
-PREFIX_V3 = {
-    "id": 1,
-    "url": "/api/ipam/prefixes/1/",
-    "prefix": "10.0.0.0/24",
+# v3.5 prefix with site
+V3_PREFIX = {
+    "id": 8452,
+    "url": "/api/ipam/prefixes/8452/",
+    "prefix": "10.23.34.0/24",
+    "family": {"label": "IPv4", "value": 4},
     "site": {
-        "id": 2,
-        "url": "/api/dcim/sites/2/",
         "display": "TST1",
-        "name": "TST1",
-        "slug": "tst1"
-    },
-}
-
-# ipam/prefixes data for Netbox >= v4.2
-PREFIX_V4 = {
-    "id": 1,
-    "url": "/api/ipam/prefixes/1/",
-    "prefix": "10.0.0.0/24",
-    "scope": {
-        "id": 2,
-        "url": "/api/dcim/sites/2/",
-        "display": "TST1",
+        "id": 168,
         "name": "TST1",
         "slug": "tst1",
-        "description": ""
+        "url": "/api/dcim/sites/168/",
     },
 }
 
-# Example that does not raise an error (not recommended)
-site: str = PREFIX_V3.get("site", {}).get("name", "")  # aceptable in Netbox < v4.2
-print(f"{site=}")
-site: str = PREFIX_V4.get("site", {}).get("name", "")  # not aceptable in Netbox >= v4.2
-print(f"{site=}")
-# site="TST1"
-# site=""
+# v3.5 prefix without site
+V3_PREFIX_ = {
+    "id": 8452,
+    "url": "/api/ipam/prefixes/8452/",
+    "prefix": "10.23.34.0/24",
+    "family": {"label": "IPv4", "value": 4},
+    "site": None,
+}
 
-# Example that raises an error (recommended)
+# v4.3 prefix with site
+V4_PREFIX = {
+    "id": 8452,
+    "url": "/api/ipam/prefixes/8452/",
+    "prefix": "10.23.34.0/24",
+    "family": {"label": "IPv4", "value": 4},
+    "scope_type": "dcim.site",
+    "scope_id": 168,
+    "scope": {
+        "description": "",
+        "display": "TST1",
+        "id": 168,
+        "name": "TST1",
+        "slug": "tst1",
+        "url": "/api/dcim/sites/168/",
+    },
+}
+
+# difference between v3.5 and v4.3
+pprint(list(dictdiffer.diff(V3_PREFIX, V4_PREFIX)), width=120)
+print()
+# [("add",
+#   "",
+#   [("scope_type", "dcim.site"),
+#    ("scope_id", 168),
+#    ("scope",
+#     {"display": "TST1", "id": 168, "name": "TST1", "slug": "tst1"})]),
+#  ("remove",
+#   "",
+#   [("site",
+#     {"display": "TST1", "id": 168, "name": "TST1", "slug": "tst1"})])]
+
+
+# get site name
+site3_1 = V3_PREFIX["site"]["name"]
+site3_2 = V3_PREFIX.get("site", {}).get("name", "")
+site3_3 = str(dict(V3_PREFIX_.get("site") or {}).get("name") or "")
+site3_4 = str(dict(V4_PREFIX.get("site") or {}).get("name") or "")
+print(f"{site3_1=}")
+print(f"{site3_2=}")
+print(f"{site3_3=}")
+print(f"{site3_4=}")
+print()
+# site3_1='TST1'
+# site3_2='TST1'
+# site3_3=''
+# site3_4=''
+
+# parse site name
 try:
-    site: str = NbValue(PREFIX_V3).str("site", "name")
-    print(f"{site=}")
-except NbVersionError as ex:
-    print(ex)
-# Deprecated model "ipam/prefixes.site" in /api/ipam/prefixes/1/, please use "ipam/prefixes.scope".
+    site4_1 = NbValue(V3_PREFIX).str("site", "name")
+    print(f"{site4_1=}")
+except Exception as ex:
+    print(f"site4_1: {ex}")
+site4_2 = NbValue(V3_PREFIX).site_name()
+site4_3 = NbValue(V4_PREFIX).site_name()
+print(f"{site4_2=}")
+print(f"{site4_3=}")
+# site4_1: Deprecated model 'ipam/prefixes.site' in /api/ipam/prefixes/8452/, expected 'ipam/prefixes.scope'.
+# site4_2='TST1'
+# site4_3='TST1'
