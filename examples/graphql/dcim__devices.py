bca26e0f6dd8ab09004f0f95f6c8e39d7fda26e3
Vladimir Prusakov <dzhedaj@gmail.com>
2026-03-09 15:46:26 +0200

    examples

--- examples/graphql/dcim__devices.py
+++ examples/graphql/dcim__devices.py
@@ -1,9 +1,7 @@
 """Example NbApi.dcim.devices.graphql()."""
 import logging
 
-from requests import HTTPError
-
-from nbforager import NbApi, NbForager
+from nbforager import NbApi
 
 # Enable logging DEBUG mode
 logging.getLogger().setLevel(logging.DEBUG)
@@ -13,20 +11,36 @@ HOST = "demo.netbox.dev"
 TOKEN = "1234567890123456789012345678901234567890"
 nb = NbApi(host=HOST, token=TOKEN)
 
-# # All dcim/devices
-# devices = nb.dcim.devices.graphql(fields="id name")
-# print(devices)  # [{"id": 1, "name": "DEVICE1"}, ...
+# All dcim/devices
+devices = nb.dcim.devices.graphql(fields="id name")
+print(devices)  # [{"id": 1, "name": "DEVICE1"}, ...
 
 # Filter dcim/devices
-fields="id name primary_ip4 { id address } tenant { id slug }"
-filters='''
+fields="""
+id
+name
+primary_ip4 { id address }
+tenant { id slug }
+platform { id slug }
+role { id slug }
+"""
+filters_v454='''
 {
     status: {exact: STATUS_ACTIVE},
     primary_ip4: {id: {is_null: false}},
     tenant: {slug: {exact: "dunder-mifflin"}},
+    platform: {slug: {exact: "cisco-ios"}},
+    role: {slug: {exact: "router"}},
 }
 '''
-devices = nb.dcim.devices.graphql(fields=fields, filters=filters)
+filters_v446='''
+{
+    status: STATUS_ACTIVE,
+    primary_ip4: {address: {is_null: false}},
+    tenant: {slug: {in_list: ["itbo-noc", "noc"]}},
+    platform: {slug: {exact: "paloalto_panos"}},
+    role: {slug: {exact: "firewall"}},
+}
+'''
+devices = nb.dcim.devices.graphql(fields=fields, filters=filters_v454)
 print(devices)  # [{"id": 1, "name": "DEVICE1"}, ...
-xx = [d for d in devices if d["id"] == "108"]
-x = 1
